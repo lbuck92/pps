2ecd0b65f6a4a6c64781443482d7c4f8bf140b33
Irisa Myint <irisa.t.myint@vanderbilt.edu>
2019-09-02 11:19:30 -0500

    randomized trials

--- Assets/Character_Driver.cs
+++ Assets/Character_Driver.cs
@@ -2,141 +2,157 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character_Driver : MonoBehaviour {
+public class Character_Driver : MonoBehaviour
+{
 
-	public GameObject female_regular, male_regular, male_intimidating, female_intimidating, monster, cameraEye;
-	GameObject active_object;
-	Rigidbody current_body;
-	float[] targetPositions = new float[5]{-1f,-0.7f,-1.5f,-1.2f,-2f};
-	int[] trials = new int[30]{1,2,3,4,5,4,2,3,1,5,3,4,5,2,1,2,3,4,5,1,5,4,3,2,1,4,5,1,2,3};
-	int curTargetArr, soloPosition, currentAvatar = 0;
-	public static int trialNumber = 1;
-	public static string active_character;
-	public static float active_distance;
-	public static bool trigger_pressed, avatar_walking, can_start, can_increment = false;
-	public static bool trial_incomplete = true;
+    public GameObject female_regular, male_regular, male_intimidating, female_intimidating, monster, cameraEye;
+    GameObject active_object;
+    Rigidbody current_body;
+    float[] targetPositions = new float[5] { -1f, -0.7f, -1.5f, -1.2f, -2f };
+    int[] trials = new int[5] { 1, 2, 3, 4, 5 };
+    int curTargetArr, soloPosition, currentAvatar, increment_counter = 0;
+    public static int trialNumber = 1;
+    public static string active_character;
+    public static float active_distance;
+    public static bool trigger_pressed, avatar_walking, can_start, can_increment, can_mod, can_mod2 = false;
+    public static bool trial_incomplete = true;
 
-	void Update () {
+    void Update()
+    {
 
-		if (Input.GetKeyDown (KeyCode.S))
-			can_start = true;
+        Debug.Log(trialNumber);
 
-		if (soloPosition > 29)
-			cameraEye.SetActive (false);
+        if (Input.GetKeyDown(KeyCode.S)) {
+            can_start = true;
+            shuffleTrials(trials);
+        }
+        
+        if(increment_counter == 4){
+            can_mod = true;
+            increment_counter = 0;
+        }
 
-		currentAvatar = trials [soloPosition];
+        //quit the application when you've reached the final trial
+        if (trialNumber == 126){
+            
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+      			Application.Quit();
+#endif
+        }else if(trialNumber % 5 == 0 && can_mod){
+            can_mod = false;
+            shuffleTrials(trials);
+            curTargetArr++;
+            soloPosition = 0;
+            if (curTargetArr == 5)
+                curTargetArr = 0;
+        }
 
-		if (currentAvatar == 1) {
-			active_object = female_regular;
-		} else if (currentAvatar == 2) {
-			active_object = male_regular;
-		} else if (currentAvatar == 3) {
-			active_object = male_intimidating;
-		} else if (currentAvatar == 4) {
-			active_object = female_intimidating;
-		} else if (currentAvatar == 5) {
-			active_object = monster;
-		}
+        
+        currentAvatar = trials[soloPosition];
 
-		if (active_object != null)
-			active_character = active_object.name;
+        if (currentAvatar == 1){
+            active_object = female_regular;
+        }else if (currentAvatar == 2){
+            active_object = male_regular;
+        }else if (currentAvatar == 3){
+            active_object = male_intimidating;
+        }else if (currentAvatar == 4){
+            active_object = female_intimidating;
+        }else if (currentAvatar == 5){
+            active_object = monster;
+        }
 
-		if (!trigger_pressed && !avatar_walking && can_start) {
-			StartCoroutine (waitforwalk ());
-		}
+        if (active_object != null)
+            active_character = active_object.name;
 
-		//choose current character (you can only choose current character when all trials have been run, i.e., when curTargetArr == 0)
-		//remove comments if you want to run the study manually.
-		/*if (Input.GetKeyDown (KeyCode.Alpha1) && curTargetArr == 0) {
-			active_object = female_regular;
-			active_character = active_object.name;
-		} else if (Input.GetKeyDown (KeyCode.Alpha2) && curTargetArr == 0) {
-			active_object = male_regular;
-			active_character = active_object.name;
-		} else if (Input.GetKeyDown (KeyCode.Alpha3) && curTargetArr == 0) {
-			active_object = male_intimidating;
-			active_character = active_object.name;
-		} else if (Input.GetKeyDown (KeyCode.Alpha4) && curTargetArr == 0) {
-			active_object = female_intimidating;
-			active_character = active_object.name;
-		} else if (Input.GetKeyDown (KeyCode.Alpha5) && curTargetArr == 0) {
-			active_object = monster;
-			active_character = active_object.name;
-		} else if (active_object == null)
-			return;*/
+        if (!trigger_pressed && !avatar_walking && can_start) 
+            StartCoroutine(waitforwalk());
+       
 
-		//drive character forward (give character a velocity > 0)
-		if (active_object != null && Input.GetKeyDown (KeyCode.Space))
-			driveCharacterForward ();
 
-		//character reset to original position with velocity 0 when trigger is pressed and increments the trial number
-		if (Controller_Driver.triggerPressed && !trigger_pressed) {
-			trigger_pressed = true;
-			can_increment = true;
-			trialNumber++;
-			curTargetArr++;
-			if (curTargetArr == 5) {
-				curTargetArr = 0;
-				soloPosition++;
-			}
-			characterReset ();
-		}
+        //character reset to original position with velocity 0 when trigger is pressed and increments the trial number
+        if (Controller_Driver.triggerPressed && !trigger_pressed)
+        {
+            trigger_pressed = true;
+            can_increment = true;
+            increment_counter++;
+            trialNumber++;
+            soloPosition++;
+            characterReset();
+        }
 
-		//disallows the user from pressing the trigger prematurely before the next trial
-		if (trigger_pressed)
-			StartCoroutine (waitfornextresponse ());
+        //disallows the user from pressing the trigger prematurely before the next trial
+        if (trigger_pressed)
+            StartCoroutine(waitfornextresponse());
 
-		if(!trigger_pressed && active_object.transform.position.x > 3)
-			characterReset();
+        if (!trigger_pressed && active_object.transform.position.x > 3)
+            characterReset();
 
-		updateCharacterPos ();
-	}
+        updateCharacterPos();
+    }
 
-	void driveCharacterForward(){
-		current_body = active_object.GetComponent<Rigidbody> ();
-		current_body.velocity = ((transform.position * -0.75f) / transform.position.magnitude);
-		avatar_walking = true;
-	}
+    void driveCharacterForward()
+    {
+        current_body = active_object.GetComponent<Rigidbody>();
+        current_body.velocity = ((transform.position * -0.75f) / transform.position.magnitude);
+        avatar_walking = true;
+    }
 
-	void characterReset(){
-		active_object.transform.position = new Vector3 (-3.5f, active_object.transform.position.y, active_object.transform.position.z);
-		current_body.velocity = new Vector3 (0f, 0f, 0f);
-		if (Controller_Driver.triggerPressed) {
-			Data_Management.recordData ();
-			Controller_Driver.triggerPressed = false;
-		}
-		avatar_walking = false;
-	}
+    void characterReset()
+    {
+        active_object.transform.position = new Vector3(-3.5f, active_object.transform.position.y, active_object.transform.position.z);
+        current_body.velocity = new Vector3(0f, 0f, 0f);
+        if (Controller_Driver.triggerPressed)
+        {
+            Data_Management.recordData();
+            Controller_Driver.triggerPressed = false;
+        }
+        avatar_walking = false;
+    }
 
-	void updateCharacterPos(){
+    void updateCharacterPos()
+    {
 
-		if (active_object.transform.position.x > (targetPositions[curTargetArr] - 0.025) && 
-			active_object.transform.position.x < (targetPositions[curTargetArr] + 0.025)) {
-			Controller_Driver.hapticPulse ();
-			active_distance = targetPositions [curTargetArr];
-			Debug.Log ("Dist no. " + (curTargetArr+1));
-			if (curTargetArr == 5) {
-				Debug.Log ("MAKE A CHARACTER SWITCH");
-			}
-		}
-	}
+        if (active_object.transform.position.x > (targetPositions[curTargetArr] - 0.025) &&
+            active_object.transform.position.x < (targetPositions[curTargetArr] + 0.025))
+        {
+            Controller_Driver.hapticPulse();
+            active_distance = targetPositions[curTargetArr];
+        }
+    }
 
-	public static string getAndReturnActiveCharacter(){
+    public static string getAndReturnActiveCharacter()
+    {
 
-		return active_character;
+        return active_character;
 
-	}
+    }
 
-	IEnumerator waitfornextresponse(){
+    void shuffleTrials(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int newPosition = Random.Range(0, 4);
+            int tmp = arr[i];
+            arr[i] = arr[newPosition];
+            arr[newPosition] = tmp;
+        }
+    }
 
-		yield return new WaitForSeconds (1.5f);
-		trigger_pressed = false;
-	}
+    IEnumerator waitfornextresponse()
+    {
 
-	IEnumerator waitforwalk(){
+        yield return new WaitForSeconds(1.5f);
+        trigger_pressed = false;
+    }
 
-		yield return new WaitForSeconds (1.5f);
-		driveCharacterForward ();
-	}
-		
+    IEnumerator waitforwalk()
+    {
+
+        yield return new WaitForSeconds(1.5f);
+        driveCharacterForward();
+    }
+
 }
